a990b4d6c00c35583be00e7f4e6667967ac10dc5
Dilanka Yasith <dilanka.pemarathne@nobletec.co>
2025-08-30 15:06:27 +0530

    commit 2

---
+++ app/users/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Interface } from 'readline';
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+  console.log(users);
+  return (
+    <>
+    <h1>Users</h1>
+    <ul>
+      {users.map(user => <li key={user.id}>{user.id+" "+user.name}</li>)}
+    </ul>
+    </>
+  )
+}
+
+export default UsersPage
